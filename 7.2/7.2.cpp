d6db95d30ece26e00a068c8ce09a5b6857fe7e01
BGBro <bohdan.horbachov.itis.2020@lpnu.ua>
2021-05-31 14:03:08 +0300

    master

---
+++ 7.2/7.2.cpp
@@ -0,0 +1,34 @@
+﻿// обчислити середнє арифметичне непарних елементів
+// спосіб 2 - використання алгоритмів STL
+#include <iostream> // std::cout
+#include <algorithm> // std::generate
+#include <vector> // std::vector
+#include <ctime> // std::time
+#include <numeric> // std::accumulate
+using namespace std;
+// function generator:
+int RandomNumber() { return (std::rand() % 100); }
+void print(vector<int> a)
+{
+	for (int i : a)
+		cout << i << " ";
+	cout << endl;
+}
+int main()
+{
+	srand(unsigned(time(0)));
+	int n;
+	cout << "n = ? "; cin >> n;
+	vector<int> v(n);
+	generate(v.begin(), v.end(), RandomNumber);
+	print(v);
+	vector<int> u(v.size());
+	auto p = copy_if(v.begin(), v.end(), u.begin(), [](int i) {return i % 2 != 0; });
+
+	u.resize(p - u.begin());
+	print(u);
+	auto k = u.size();
+	auto s = accumulate(u.begin(), u.end(), 0);
+	cout << "AVG = " << s << endl;
+	return 0;
+}
